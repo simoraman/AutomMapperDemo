1ef604fed2825b4c12fc159bcb14782e326d65c9
Simo Råman <simoraman@gmail.com>
2013-01-03 07:46:06 +0200

    init

---
+++ AutomMapperDemo/MappingTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using AutoMapper;
+
+namespace AutomMapperDemo
+{
+
+    [TestFixture]
+    public class MappingTests
+    {
+        [Test]
+        public void TestConventions() {
+            Mapper.CreateMap<Person, PersonDTO>();
+            Person p = new Person { FirstName="Bruce", LastName="Wayne", SecretId="Batman" };
+            PersonDTO personDto = Mapper.Map<Person, PersonDTO>(p);
+            Assert.That(personDto.FirstName, Is.EqualTo("Bruce"));
+            Assert.That(personDto.LastName, Is.EqualTo("Wayne"));
+        }
+
+    }
+}
---
+++ AutomMapperDemo/PersonDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AutomMapperDemo
+{
+    class PersonDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
